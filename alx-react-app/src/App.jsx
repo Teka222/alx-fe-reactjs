6725798cf5aea4047d8824ec8e799b83fdca51b7
Teka222 <tekanonetflorist01@gmail.com>
2024-11-10 20:46:06 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage'; // Make sure this import is correct
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div>
-      <WelcomeMessage /> {/* Ensure this component is included here */}
-    </div>
-  );
+    return (
+        <div>
+            <Header /> {/* Header component */}
+            <MainContent /> {/* MainContent component */}
+            <Footer /> {/* Footer component */}
+        </div>
+    );
 }
 
 export default App;
