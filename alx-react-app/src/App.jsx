8df22a8747165c1e712d5d234804ad3987a616f7
Teka222 <tekanonetflorist01@gmail.com>
2024-11-10 20:36:48 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import WelcomeMessage from './components/WelcomeMessage';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
