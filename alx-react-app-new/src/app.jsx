d8808e35eddc88d8b46c1de7e1bc993beff865f5
Teka222 <tekanonetflorist01@gmail.com>
2024-11-17 20:56:36 +0200

    Create app.jsx

---
+++ alx-react-app-new/src/app.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="John Doe" age={30} bio="A React enthusiast." />
+      <Counter />
+      <Footer />
+    </div>
+  );
+}
