fca4a322f36e01c331f33f4dddffed872fe81195
Teka222 <tekanonetflorist01@gmail.com>
2024-11-17 20:58:12 +0200

    Update app.jsx

--- alx-react-app-new/src/app.jsx
+++ alx-react-app-new/src/app.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
+
 function App() {
   return (
     <div>
@@ -9,3 +16,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
