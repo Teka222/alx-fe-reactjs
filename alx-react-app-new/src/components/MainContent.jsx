f01b63a27a74840e3a175a9d7a67bbb88320f936
Teka222 <tekanonetflorist01@gmail.com>
2024-11-17 20:50:12 +0200

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f8ff' }}>
+      <p>Welcome to the main content area!</p>
+    </main>
+  );
+}
+
+export default MainContent;
