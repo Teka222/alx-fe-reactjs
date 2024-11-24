e7889319aec3b6e4239ca9f9a9975915d3644fce
Teka222 <tekanonetflorist01@gmail.com>
2024-11-25 00:00:43 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,13 @@
 // src/App.jsx
+import React from 'react';
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext'; // Import the UserContext
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}> {/* Provide context value */}
+    <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
   );
