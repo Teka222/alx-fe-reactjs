261443a2a8b93d452b6e3eb468d948aff9813236
Teka222 <tekanonetflorist01@gmail.com>
2024-11-24 23:54:14 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,16 +1,13 @@
 // src/App.jsx
-import React from 'react';
-import ProfilePage from './ProfilePage'; // Import ProfilePage component
+import ProfilePage from './ProfilePage';
 import UserContext from './UserContext'; // Import the UserContext
 
 function App() {
-  // This is the data we want to share across components
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    // Wrap ProfilePage with the Provider to share userData
-    <UserContext.Provider value={userData}>
-      <ProfilePage />  {/* Now ProfilePage and its children can access userData */}
+    <UserContext.Provider value={userData}> {/* Provide context value */}
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
