a5f89b3084e037e68e90474bf9e696022e207b9d
Teka222 <tekanonetflorist01@gmail.com>
2024-11-24 23:48:45 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,46 +1,11 @@
 import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
-  favorites: [],
-  recommendations: [],
-  
-  // Add a new recipe
-  addRecipe: (newRecipe) => set((state) => ({
-    recipes: [...state.recipes, newRecipe],
-  })),
+  recipes: [], // Stores all recipes
+  favorites: [], // Stores favorite recipe IDs
+  recommendations: [], // Stores recommended recipes
+  searchTerm: '', // Stores the current search term
+  filteredRecipes: [], // Stores filtered recipes based on the search term
 
-  // Update an existing recipe
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((recipe) =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-  })),
-
-  // Delete a recipe by ID
-  deleteRecipe: (recipeId) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-  })),
-
-  // Add a recipe to favorites
-  addFavorite: (recipeId) => set((state) => ({
-    favorites: [...state.favorites, recipeId],
-  })),
-
-  // Remove a recipe from favorites
-  removeFavorite: (recipeId) => set((state) => ({
-    favorites: state.favorites.filter((id) => id !== recipeId),
-  })),
-
-  // Generate recommendations based on favorites
-  generateRecommendations: () => set((state) => {
-    // Simple recommendation logic (you can expand this)
-    const recommended = state.recipes.filter((recipe) =>
-      state.favorites.includes(recipe.id) && Math.random() > 0.5
-    );
-    return { recommendations: recommended };
-  }),
-
-  // Set all recipes
-  s
-
+  // Action to add a recipe to the state
+  addRecipe: (newRecipe) => set((stat
