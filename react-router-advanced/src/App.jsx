fd469090ae79a404dd24a2ac7b64d59cab29de28
Teka222 <tekanonetflorist01@gmail.com>
2024-11-24 21:51:29 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/ src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';  // Import the BlogPost component
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />  {/* Add dynamic route */}
       </Routes>
     </Router>
   );
 };
-
-const Home = () => <div>Home Page</div>;
-const About = () => <div>About Page</div>;
 
 export default App;
