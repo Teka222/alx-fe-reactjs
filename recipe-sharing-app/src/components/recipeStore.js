f3228be5ceafb59e7051d39d3cadd23d64d996d4
Teka222 <tekanonetflorist01@gmail.com>
2024-11-24 23:24:43 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,26 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  
+  // Add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  
+  // Delete a recipe by ID
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Update a recipe by ID
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  // Set the initial list of recipes
   setRecipes: (recipes) => set({ recipes }),
 }));
 
