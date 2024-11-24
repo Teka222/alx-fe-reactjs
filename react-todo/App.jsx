bf9684b86f43cb3fa8ee9b3ce7d53c8a0bb6d0cb
Teka222 <tekanonetflorist01@gmail.com>
2024-11-24 22:10:53 +0200

    Create App.jsx

---
+++ react-todo/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
