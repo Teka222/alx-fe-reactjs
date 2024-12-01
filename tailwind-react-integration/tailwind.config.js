d2af6f33786c75b05bc9d009034c79d11ea4d73e
Teka222 <tekanonetflorist01@gmail.com>
2024-12-01 22:35:23 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to your templates
   theme: {
     extend: {},
   },
