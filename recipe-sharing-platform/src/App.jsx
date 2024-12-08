01e9e1b9c441d0e6ab7981da7323dda605ac30f8
Teka222 <tekanonetflorist01@gmail.com>
2024-12-08 20:29:44 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,5 +1,16 @@
+fimport { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+
 function App() {
-  return <h1 className="text-blue-500 text-center">Tailwind is Working!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
