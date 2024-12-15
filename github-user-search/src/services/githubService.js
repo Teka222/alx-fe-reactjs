b3213271d0ca958b79b0500a465ba8253c848f4d
Teka222 <tekanonetflorist01@gmail.com>
2024-12-15 21:27:24 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users';
+export const fetchAdvancedSearchResults = async ({ username, location, minRepos }) => {
+  let query = [];
+  if (username) query.push(`${username} in:login`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>=${minRepos}`);
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/${username}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('User not found');
-  }
+  const queryString = query.join('+');
+  const url = `https://api.github.com/search/users?q=${queryString}`;
+
+  const response = await axios.get(url);
+  return response.data; // Returns users list under `items`
 };
